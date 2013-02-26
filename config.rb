5685cf6e9dc4715318f2d9c489cdf97e2db4b227
unknown <ben.edwards@LDNMRM-LW71641.emea.corp.ipgnetwork.com>
2013-02-26 17:59:39 +0000

    Initial styles

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'breakpoint'
+require 'normalize'
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+output_style = :compressed
+line_comments = false
+asset_cache_buster :none
