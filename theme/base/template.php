bdef80b5fc171c01c9269d3507b9dbe362649d09
benedfit <hello@benedfit.com>
2013-04-25 15:38:50 +0100

    Tracking

--- theme/base/template.php
+++ theme/base/template.php
@@ -27,6 +27,16 @@
 				<footer id="footer">&copy; <a href="/">Ben Edwards</a>. All rights reserved.</footer>
 			</div>
 		</div>
-		<script>var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-34470231-1"]);_gaq.push(["_trackPageview"]);(function(){var b=document.createElement("script");b.type="text/javascript";b.async=true;b.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(b,a)})();</script>
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-34470231-1']);
+		  _gaq.push(['_setDomainName', 'benedfit.com']);
+		  _gaq.push(['_trackPageview']);		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
 	</body>
 </html>
