bd95ca4b8ab4fe690dbd6ae7dd32dd750a42e7bf
benedfit <hello@benedfit.com>
2013-04-25 15:32:02 +0100

    Tracking

--- theme/base/template.php
+++ theme/base/template.php
@@ -27,5 +27,6 @@
 				<footer id="footer">&copy; <a href="/">Ben Edwards</a>. All rights reserved.</footer>
 			</div>
 		</div>
+		<script>var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-34470231-1"]);_gaq.push(["_trackPageview"]);(function(){var b=document.createElement("script");b.type="text/javascript";b.async=true;b.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(b,a)})();</script>
 	</body>
 </html>
