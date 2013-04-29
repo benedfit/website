2505e12413ad7ba1a717642329fee4169b9e0973
benedfit <hello@benedfit.com>
2013-04-29 17:46:35 +0100

    Config tweak

--- config.rb
+++ config.rb
@@ -3,9 +3,9 @@ require 'breakpoint'
 http_path = "/"
 css_dir = "_deploy/css"
 fonts_dir = "_deploy/fonts"
-http_fonts_path = "fonts"
+http_fonts_path = "../fonts"
 images_dir = "_deploy/img"
-http_images_path = "img"
+http_images_path = "../img"
 sass_dir = "sass"
 output_style = :compressed
 line_comments = false
