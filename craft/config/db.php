0cc00a1c8a4f94cfc37d89609f643c325e2c17f8
benedfit <hello@benedfit.com>
2013-07-01 17:48:10 +0100

    Craft deployment

--- craft/config/db.php
+++ craft/config/db.php
@@ -6,39 +6,24 @@
  * All of your system's database configuration settings go in here.
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
- 
-/**
- * $mysql_host = "mysql2.000webhost.com";
- * $mysql_database = "a7708405_bndf";
- * $mysql_user = "a7708405_bndf";
- * $mysql_password = "Qvsau6v1983";
- */
 
-return array(
+$whitelist = array('benedfit.benedfit.dev');
 
-/**
- * The database server name or IP address. Usually 'localhost' or '127.0.0.1'.
- */
-'server' => 'localhost',
-
-/**
- * The database username to connect with.
- */
-'user' => 'benedfit',
-
-/**
- * The database password to connect with.
- */
-'password' => 'Qvsau6v1983',
-
-/**
- * The name of the database to select.
- */
-'database' => 'benedfit',
-
-/**
- * The prefix to use when naming tables. This can be no more than 5 characters.
- */
-'tablePrefix' => 'craft',
-
-);
+if(in_array($_SERVER['HTTP_HOST'], $whitelist)) {
+	return array(
+	'server' => 'localhost',
+	'user' => 'benedfit',
+	'password' => 'Qvsau6v1983',
+	'database' => 'benedfit',
+	'tablePrefix' => 'craft',
+	);
+}
+else {
+	return array(
+	'server' => 'mysql.serversfree.com',
+	'user' => 'u984548764_bndf',
+	'password' => 'Qvsau6v1983',
+	'database' => 'u984548764_bndf',
+	'tablePrefix' => 'craft',
+	);
+}
