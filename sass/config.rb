d58caf31b4179efee3adf62ad0c25d22d9b20b1d
benedfit <hello@benedfit.com>
2013-07-03 18:02:45 +0100

    Initial search implementation

--- sass/config.rb
+++ sass/config.rb
@@ -1,13 +1,13 @@
 require 'breakpoint'
 
 http_path = "/"
-css_dir = "source/css"
-fonts_dir = "source/fonts"
+css_dir = "../source/css"
+fonts_dir = "../source/fonts"
 http_fonts_path = "/fonts"
-images_dir = "source/img"
+images_dir = "../source/img"
 http_images_path = "/img"
-sass_dir = "sass"
+sass_dir = "/"
 
-output_style = :expanded
+output_style = :compressed
 line_comments = false
 asset_cache_buster :none
