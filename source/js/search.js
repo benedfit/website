cf3c8442fa62394d836e24eb140b0a3eb58b5172
benedfit <hello@benedfit.com>
2013-07-04 11:19:03 +0100

    Search implementation

--- source/js/search.js
+++ source/js/search.js
@@ -1,38 +1,34 @@
 ---
 ---
-var docs = 
-[ 
-{% for post in site.posts limit:10 %}
-  {% include post.json %},
-{% endfor %}
-];
+var docs = [{% for post in site.posts %}{% include post.json %},{% endfor %}],
+idx = lunr(function () {
+	this.field('title', 10);
+	this.field('content');
+});
 
-var idx = lunr(function () {
-  this.field('title', 10);
-  this.field('content');
-})
-
-for(var index in docs) {
-  idx.add(docs[index]);
+for (var index in docs) {
+	idx.add(docs[index]);
 }
 
-$(function() {
-  $("#search button").click(function() {
-    search();
-  });
-  $("#search input").keypress(function(e) {
-    if(e.which == 13) {
-      e.preventDefault();
-      search();
-    }
-  });
+$(function () {
+	$('#header').removeClass('hide-search');
+	
+	$('#search button').click(function (e) {
+		e.preventDefault();
+		search();
+	});
+	
+	$("#search input").keypress(function (e) {
+		if (e.which == 13) {
+			e.preventDefault();
+			search();
+		}
+	});
 })
 
-function search() {
+function search () {
   var result = idx.search($("#search input").val());
-  if(result && result.length > 0) {
+  if (result && result.length > 0) {
     window.location.replace(result[0].ref);
-  } else {
-    alert("Found nothing");
   }
 }
