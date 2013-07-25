22e5f0262f9083aad75002a253632cee8428032b
benedfit <hello@benedfit.com>
2013-07-25 10:46:47 +0100

    Setup wercker

---
+++ config.rb
@@ -0,0 +1,14 @@
+require 'breakpoint'
+
+http_path = "/"
+css_dir = "source/css"
+http_css_path = "/css"
+fonts_dir = "source/fonts"
+http_fonts_path = "/css"
+images_dir = "source/img"
+http_images_path = "/css"
+sass_dir = "source/_sass"
+
+output_style = :compressed
+line_comments = false
+asset_cache_buster :none
