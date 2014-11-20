0854e80b6d47b4d1eea5985ff2e745765e1bea58
Ben Edwards <ben.edwards@clock.co.uk>
2014-11-20 14:06:36 +0000

    WIP on posts and pages

--- pliers/build-html.js
+++ pliers/build-html.js
@@ -14,53 +14,102 @@ function task(pliers, config) {
 
   pliers('buildHtml', function (done) {
 
-    async.each(pliers.filesets.pages, function (file) {
+    var options = { pretty: false }
+      , pages = []
+      , posts = []
+
+    merge(options, config)
+
+    async.each(pliers.filesets.pages, function (file, callback) {
       var dest = file.replace(config.src, config.dest)
         , ext = path.extname(file)
-        , isMarkdown = ext === '.md'
         , properties
-        , options = { pretty: false }
-
-      mkdir(path.dirname(dest))
+        , page = {}
 
       fs.readFile(file, 'utf8', function (err, data) {
-        if (err) {
-          pliers.logger.error('Failed to render contents')
-          pliers.logger.error(err.stack)
-        }
+        if (err) throw(err)
 
         if (frontmatter.test(data)) {
           properties = frontmatter(data)
 
-          merge(options, config)
-          merge(options, properties.attributes)
+          merge(page, properties.attributes)
 
-          if (properties.attributes.layout) {
-            file = join(__dirname, '..', config.src, 'views', properties.attributes.layout + '.jade')
-
-            if (isMarkdown) {
-              options.contents = namp(properties.body).html
-            } else {
-              options.contents = properties.body
-            }
-
-            data = jade.renderFile(file, options)
+          if (ext === '.md') {
+            page.contents = namp(properties.body).html
+            page.parsed = true
           } else {
-            data = jade.render(properties.body, options)
+            page.contents = properties.body
           }
 
-          if (isMarkdown) {
-            dest = dest.replace(ext, '.html')
+          page.path = setDestination(dest, page)
+          page.url = page.path.replace(join(__dirname, '..', config.dest), '').replace('index.html', '')
+
+          if (page.layout === 'post') posts.push(page)
+          pages.push(page)
+        } else {
+          mkdir(path.dirname(dest))
+
+          fs.writeFile(dest, data)
+        }
+
+        callback()
+      })
+
+    }, function (err) {
+      if (err) {
+        pliers.logger.error('Failed to render contents')
+        pliers.logger.error(err.stack)
+      }
+
+      options.posts = posts
+      options.pages = pages
+
+      async.each(pages, function (page) {
+        var dest = page.path
+          , file
+          , data
+
+        mkdir(path.dirname(dest))
+
+        if (page.layout) {
+          if (!page.parsed) {
+            options.contents = jade.render(page.contents, options)
           } else {
-            dest = dest.replace('.jade', '')
+            options.contents = page.contents
           }
+
+          file = join(__dirname, '..', config.src, 'views', page.layout + '.jade')
+          data = jade.renderFile(file, options)
+        } else {
+          data = jade.render(page.contents, options)
         }
 
         fs.writeFile(dest, data)
-        done()
       })
 
+      done()
     })
+
+    function setDestination(dest, page) {
+      var permalink = page.permalink
+        , ext = path.extname(dest)
+
+      if (permalink) {
+        if (permalink.match('/$')) {
+          permalink = join(permalink, 'index.html')
+        }
+
+        dest = join(__dirname, '..', config.dest, permalink)
+      }
+
+      if (ext === '.md' || ext === '.jade') {
+        dest = dest.replace(ext, '.html')
+      } else {
+        dest = dest.replace('.jade', '')
+      }
+
+      return dest
+    }
 
   })
 
---
+++ source/posts/2013-11-21-pattern-library-annotations-documentation-and-more.md
@@ -0,0 +1,12 @@
+---
+layout: post
+title: Pattern library annotations, documentation, and more
+metadescription: Dave Rupert, Brad Frost, Geoff Barnes, Aaron Gustafson & Patrick Fulton share their thoughts on responsive deliverables, pattern libraries & annotations
+date: 2013-11-21
+permalink: /2013/11/pattern-library-annotations-documentation-and-more/
+---
+What started out as a simple chat this afternoon between myself and [Cheryl Natel](https://twitter.com/cheryl_lis) on the ways in which we could integrate functional specifications into pattern libraries spawned into a great discussion on the topic via an impromptu Google Hangout.
+
+<div class="video"><iframe src="https://www.youtube.com/embed/ROaXVB-bbek?rel=0">iframe</iframe></div>
+
+Thanks to [Dave Rupert](https://twitter.com/davatron5000), [Brad Frost](https://twitter.com/brad_frost), [Geoff Barnes](https://twitter.com/texburgher), [Aaron Gustafson](https://twitter.com/AaronGustafson) and [Patrick Fulton](https://twitter.com/patrickfulton) for coming online and sharing how they do it. And apologies to all of you that I didn't make into the Hangout, but a two prong attack from a failed internet connection and a sick child had me beat!
