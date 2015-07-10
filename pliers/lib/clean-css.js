d84d9bbc8112441f41c77953a74be46b0a796734
Ben Edwards <ben.edwards@clock.co.uk>
2015-07-10 12:20:17 +0100

    > Fix formatting

--- pliers/lib/clean-css.js
+++ pliers/lib/clean-css.js
@@ -1,9 +1,10 @@
 module.exports = function() {
-  return function(style) {
-    style = this || style;
 
+  return function(style) {
+    style = this || style
     style.on('end', function (err, css) {
-      return require('clean-css')().minify(css);
+      return require('clean-css')().minify(css)
     })
   }
+
 }
