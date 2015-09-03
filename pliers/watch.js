f1be5ab0bc8d8cd0da5e4245e0e93a89d7e30544
Ben Edwards <ben.edwards@clock.co.uk>
2015-09-03 09:46:16 +0100

    > Fix browsersync bug

--- pliers/watch.js
+++ pliers/watch.js
@@ -8,19 +8,19 @@ function createTask(pliers, config) {
 
     pliers.watch(pliers.filesets.images, function() {
       pliers.run('imagemin', function () {
-        browserSync.get(config.name).reload(pliers.filesets.pages)
+        browserSync.get(config.title).reload(pliers.filesets.pages)
       })
     })
 
     pliers.watch(pliers.filesets.stylus, function () {
       pliers.run('buildCss', function () {
-        browserSync.get(config.name).reload(pliers.filesets.pages)
+        browserSync.get(config.title).reload(pliers.filesets.pages)
       })
     })
 
     pliers.watch(pliers.filesets.src, function () {
       pliers.run('buildHtml', function () {
-        browserSync.get(config.name).reload(pliers.filesets.pages)
+        browserSync.get(config.title).reload(pliers.filesets.pages)
       })
     })
 
