d587c7a21b63eb3f969916a40642ca1ac38282ca
Ben Edwards <hello@benedfit.com>
2023-03-03 11:06:50 +0000

    chore: further updates

--- pliers/build-favicon.js
+++ pliers/build-favicon.js
@@ -1,84 +1,78 @@
-module.exports = createTask
+module.exports = createTask;
 
-var anyNewerFiles = require('any-newer-files')
-  , fs = require('fs')
-  , glob = require('glob')
-  , path = require('path')
-  , pliersImagemin = require('pliers-imagemin')
-  , rfg = require('rfg-api').init()
+var anyNewerFiles = require("any-newer-files"),
+  fs = require("fs"),
+  { globSync } = require("glob"),
+  path = require("path"),
+  pliersImagemin = require("pliers-imagemin"),
+  rfg = require("rfg-api").init();
 
-function createTask (pliers, config) {
+function createTask(pliers, config) {
+  var apiKey = "b369276b27fde847136ab1453e5953e125d6d0b6";
 
-  var apiKey = 'b369276b27fde847136ab1453e5953e125d6d0b6'
+  pliers("buildFavicon", function (done) {
+    var src = path.resolve(__dirname + "/../" + config.src),
+      dest = src + "/img",
+      sourceFile = dest + "/_favicon.png",
+      dataFile = src + "/_favicon.json",
+      request;
 
-  pliers('buildFavicon', function (done) {
-
-    var src = path.resolve(__dirname + '/../' + config.src)
-      , dest = src + '/img'
-      , sourceFile = dest + '/_favicon.png'
-      , dataFile = src + '/_favicon.json'
-      , request
-
-    if (!anyNewerFiles([ sourceFile ], [ dataFile ])) {
-      pliers.logger.warn('No Favicon changes found. No recompile required.')
-      return done()
+    if (!anyNewerFiles([sourceFile], [dataFile])) {
+      pliers.logger.warn("No Favicon changes found. No recompile required.");
+      return done();
     }
 
-    request = rfg.createRequest(
-      { apiKey: apiKey
-      , masterPicture: sourceFile
-      , iconsPath: '/img/'
-      , design:
-        { ios: { pictureAspect: 'noChange' }
-        , desktopBrowser: {}
-        , windows:
-          { pictureAspect: 'noChange'
-          , backgroundColor: config.metaTileColor
-          , onConflict: 'override'
-          }
-        , androidChrome:
-          { pictureAspect: 'noChange'
-          , themeColor: config.metaTileColor
-          , manifest:
-            { name: config.title
-            , display: 'browser'
-            , orientation: 'notSet'
-            , onConflict: 'override'
-            , declared: true
-            }
-          }
-        , safariPinnedTab:
-          { pictureAspect: 'blackAndWhite'
-          , threshold: 50
-          , themeColor: config.metaTileColor
-          }
-        }
-      , settings:
-        { 'scalingAlgorithm': 'Mitchell'
-        , 'errorOnImageTooSmall': false
-        }
-      }
-    )
+    request = rfg.createRequest({
+      apiKey: apiKey,
+      masterPicture: sourceFile,
+      iconsPath: "/img/",
+      design: {
+        ios: { pictureAspect: "noChange" },
+        desktopBrowser: {},
+        windows: {
+          pictureAspect: "noChange",
+          backgroundColor: config.metaTileColor,
+          onConflict: "override",
+        },
+        androidChrome: {
+          pictureAspect: "noChange",
+          themeColor: config.metaTileColor,
+          manifest: {
+            name: config.title,
+            display: "browser",
+            orientation: "notSet",
+            onConflict: "override",
+            declared: true,
+          },
+        },
+        safariPinnedTab: {
+          pictureAspect: "blackAndWhite",
+          threshold: 50,
+          themeColor: config.metaTileColor,
+        },
+      },
+      settings: { scalingAlgorithm: "Mitchell", errorOnImageTooSmall: false },
+    });
 
     rfg.generateFavicon(request, src, function (err, data) {
-      if (err) return done(err)
+      if (err) return done(err);
 
-      var images = glob.sync(src + '/*.{png,svg}')
+      var images = globSync(src + "/*.{png,svg}");
 
-      fs.writeFileSync(dataFile, JSON.stringify(data))
+      fs.writeFileSync(dataFile, JSON.stringify(data));
 
-      pliersImagemin(pliers, images)(function (err) {
-        if (err) return done(err)
+      pliersImagemin(
+        pliers,
+        images
+      )(function (err) {
+        if (err) return done(err);
 
         images.forEach(function (file) {
-          fs.renameSync(file, file.replace(src, dest))
-        })
+          fs.renameSync(file, file.replace(src, dest));
+        });
 
-        done()
-      })
-
-    })
-
-  })
-
+        done();
+      });
+    });
+  });
 }
